fcd8baa75be07bce954d20aa1e14b87cc09484d1
Scofield626 <765203006@qq.com>
2023-10-08 11:16:43 +0000

    aggregate latency res

---
+++ scripts/aggregate_res.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+res_path="/home/scofield/caracal/felis/results/cont0/noskew/nodep/caracal-pieces/"
+script_py="/home/scofield/caracal/felis/scripts/res-parse.py"
+res_log="agg_res.txt"
+
+if [[ -e $res_log ]]; then
+  touch $res_log
+fi
+log_path="$(pwd)/$res_log"
+
+cd $res_path
+for res in $(ls -v); do
+  if [[ -f $res ]]; then
+    echo "start process $res"
+    echo "$res" >> $log_path
+    python $script_py $res >> $log_path
+    echo "" >> $log_path
+  fi
+done
